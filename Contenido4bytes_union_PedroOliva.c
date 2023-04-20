c8ed3b08bfbacf7bac268d4b1e11a5aafbad8719
Pedro Oliva <118389547+pedritis433@users.noreply.github.com>
2023-04-20 10:13:54 -0300

    Update Contenido4bytes_union_PedroOliva.c

--- Contenido4bytes_union_PedroOliva.c
+++ Contenido4bytes_union_PedroOliva.c
@@ -10,9 +10,9 @@ int main()
     printf("Bienvenido al programa para ver el contenido de los 4 bytes de un float\nIngrese el valor flotante: ");
     scanf("%f", &var.MiNum);
     printf("\nLos bytes en hexadecimal son los siguientes:\n");
-    printf("%x\n",var.byte[0]);
-    printf("%x\n",var.byte[1]);
-    printf("%x\n",var.byte[2]);
-    printf("%x\n",var.byte[3]);
+    printf("0x%x",var.byte[3]);
+    printf("%x",var.byte[2]);
+    printf("%x",var.byte[1]);
+    printf("%x",var.byte[0]);
     return 0;
 }

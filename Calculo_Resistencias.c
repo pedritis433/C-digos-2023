925e5cf5db029165e423f763f97d4b779888eca9
Pedro Oliva <118389547+pedritis433@users.noreply.github.com>
2023-04-20 10:59:00 -0300

    Update Calculo_Resistencias.c

--- Calculo_Resistencias.c
+++ Calculo_Resistencias.c
@@ -1,19 +1,1 @@
-#include <stdio.h>
 
-float ohm (float tension, float corriente);
-
-int main(){
-   float tension,corriente,resistencia;
-    printf("Bienvenido al programa.. \n");
-    printf("Ingrese la tensión: \n");
-    scanf("%f",&tension);
-    printf("Ingrese la corriente: \n");
-    scanf("%f",&corriente);
-    resistencia = ohm (tension,corriente);
-    printf("El valor de la resistencia es de : %f ohms", resistencia);
-    return 0;
-}
-
-float ohm (float tension,float corriente){
-    return(tension/corriente);
-}

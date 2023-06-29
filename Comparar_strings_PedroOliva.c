2bd3750333eebd9e8dc538499361d15881f18d7c
Pedro Oliva <118389547+pedritis433@users.noreply.github.com>
2023-06-29 09:30:36 -0300

    Update Comparar_strings_PedroOliva.c

--- Comparar_strings_PedroOliva.c
+++ Comparar_strings_PedroOliva.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-int flag;
-void compSTR(char*, char*);
+int compSTR(char*, char*);
 
 int main()
 {
@@ -8,8 +7,8 @@ int main()
     char str2[14];
     str1[13] = 0;
     str2[13] = 0;
-    char* pstr1=&str1[0];
-    char* pstr2=&str2[0];
+    char* pstr1=str1;
+    char* pstr2=str2;
     printf("Bienvenido al programa que compara strings");
     printf("\nIngrese el primer string: ");
     scanf("%s", str1);
@@ -18,17 +17,20 @@ int main()
     
     compSTR(pstr1, pstr2);
     
-    if (flag == 1) printf("Los strings son distintos");
+    if (compSTR(pstr1, pstr2) == 1) printf("Los strings son distintos");
     else printf("Los strings son iguales");
 }
 
-void compSTR(char *pstr1,char *pstr2){
+int compSTR(char *pstr1,char *pstr2){
+    int flag = 0;
     for(int i=0; i<=13; i++){
         if (*(pstr1+i) == *(pstr2+i)){
-            flag=0;
+        flag=0;
         }else{
             flag=1;
             break;
         }
     }
+    if (flag == 1) return 1;
+    else return 0;
 }
